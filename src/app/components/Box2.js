b864df6b02bf8173ebc6438f550ba1b76c8b653a
EAlexBen <107051871+EB378@users.noreply.github.com>
2024-03-26 15:22:51 +0000

    modified:   src/app/components/Box2.js

--- src/app/components/Box2.js
+++ src/app/components/Box2.js
@@ -2,7 +2,7 @@ import styles from './Box2.module.css'
 export default function Logo() {
   return (
     <div className={styles.oa2}>
-      <img require src="#./BenniAlexanderFace.jpeg" alt="Netlify Logo" className={styles.Face} />
+      <img require src="./BenniAlexanderFace.jpeg" alt="Netlify Logo" className={styles.Face} />
       <h2>Benni Alexander, CEO</h2>
       <h5 className={styles.quotes}>"The Gopher approach is guaranteed results. We have worked for over a decade gathering real life examples of what happens when companies take <a href="#home" className={styles.a}>Business continuity</a> seriously, vs when they don't."</h5>
     </div>
