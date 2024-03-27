1b97fb6ec72768af94738d2f7bfcf7338804eea4
EAlexBen <107051871+EB378@users.noreply.github.com>
2024-03-27 09:21:33 +0000

    modified:   src/app/components/Box2.js 	modified:   src/app/components/Box3.js

--- src/app/components/Box2.js
+++ src/app/components/Box2.js
@@ -4,7 +4,7 @@ export default function Logo() {
     <div className={styles.oa2}>
       <img require src="./_81d72904-90ac-4a11-8798-f62a72168526.jpeg" alt="IMAGE  TBD" className={styles.Face} />
       <h2>SMTH</h2>
-      <h5 className={styles.quotes}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim d.</h5>
+      <h5 className={styles.quotes}>"This is a test website for a school project. This domain is in use. Site last update 27.03.2024._____________________________________________________ </h5>
     </div>
   )
 }
--- src/app/components/Box3.js
+++ src/app/components/Box3.js
@@ -2,9 +2,8 @@ import styles from './Box3.module.css'
 export default function Box1() {
   return (
     <div className={styles.oa3}>
-        <h2>When crisis hits… !!!</h2>
-        <h2 className={styles.colour}>The truth of resilience comes out. </h2>
-        <h2>— CEO</h2>
+        <h2>Start-ups are destined to fail !!!</h2>
+        <h2 className={styles.colour}>They need the systems of the billion dollar companies to become successful.</h2>
     </div>
   )
 }
