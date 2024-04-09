2a6c8c45dbbbf70e90bd95a92045adbb16f1c686
EAlexBen <107051871+EB378@users.noreply.github.com>
2024-04-09 17:02:29 +0000

    modified:   src/app/layout.js 	modified:   src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,4 @@
 import styles from "./page.module.css";
-import Nav from '@/app/components/Nav';
-import Footer from '@/app/components/Footer';
 import Homepage from "@/app/components/Homepage";
 import "./globals.css";
 
@@ -8,11 +6,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className="App">
-        <header className="App-header">
-          <Nav/>
-        </header>
         <Homepage/>
-        <Footer/>
       </div>
     </main>
   );
